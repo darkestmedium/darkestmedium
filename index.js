d01cb972d780c71618be5512fe6aa9b7c9302946
Darkest Medium <darkestmedium@pm.me>
2024-02-27 10:10:27 +0100

    Adds separate js files for button behaviour.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// Learn More Button
+document.getElementById("learn-more").addEventListener("click", function() {window.scrollTo({top: window.scrollY = 1000, behavior: 'smooth'});});
+
+// Resume Button
+document.getElementById("resume").addEventListener("click", function() {window.location.href = "./public/resume.html";});
+
+// Contact Me Button
+document.getElementById("contactme").addEventListener("click", function() {window.location.href = "mailto:darkestmedium@pm.me";});
---
+++ public/resume.js
@@ -0,0 +1,2 @@
+// Back to Main Button
+document.getElementById("back").addEventListener("click", function() {window.location.href = "../index.html";});
