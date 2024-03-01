e6215fa8bce24d8cf920c41379e7c274a4bc7dcd
Darkest Medium <darkestmedium@pm.me>
2024-03-01 22:33:33 +0100

    Cleans-up main page, splits resume back to seprate html file.

--- public/resume.js
+++ public/resume.js
@@ -1,5 +1,6 @@
 // Back to Main Button
-$("#back").click(function() {
+$("#btn-back-to-main").click(function() {
   window.location.href = "../index.html";
+  // window.scrollTo({top: window.scrollY = 758, behavior: 'smooth'});
 });
 // document.getElementById("back").addEventListener("click", function() {window.location.href = "../index.html";});
