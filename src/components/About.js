a706076087a45e459fa73c280ff07cce1d8deeaa
Darkest Medium <darkestmedium@pm.me>
2024-03-11 21:30:04 +0100

    About Rresume button fix with useNavigate from react-router-dom

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import imgAvatar from '../assets/images/me.png';
 
@@ -7,10 +7,13 @@ import imgAvatar from '../assets/images/me.png';
 
 
 function About() {
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    window.location.href = "/darkestmedium/resume";
+    navigate("/resume");
+    window.scrollTo({top: 0, behavior: 'smooth'});
   };
+
 
   return (
     <div className="sec-about-me">
@@ -31,7 +34,7 @@ function About() {
           </p>
         </div>
         <div>
-        <button onClick={handleClick}>
+          <button onClick={handleClick}>
             RESUME
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
