f49c7c82a0f9e1ef7b6cad6dbf810d4948054164
Darkest Medium <darkestmedium@pm.me>
2024-03-13 19:51:49 +0100

    Replaces BrowserRouter with HashRouter

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // Components
 import Nav from './Navbar';
@@ -12,14 +12,15 @@ function App() {
 
   return (
     <div className='root'>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+      <HashRouter>
         <Nav />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/resume" element={<Resume />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
