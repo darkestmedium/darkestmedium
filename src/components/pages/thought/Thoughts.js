476d7a8e242f1dad62703e9c077a9f4688dda1a0
Darkest Medium <darkestmedium@pm.me>
2024-03-23 23:59:22 +0100

    Adds new "thoughts"

--- src/components/pages/thought/Thoughts.js
+++ src/components/pages/thought/Thoughts.js
@@ -29,6 +29,111 @@ const thoughtEntries = [
     quoted: "- Your Competition",
     content: "",
   },
+  {
+    id: 5,
+    title: "“Contract based world.”",
+    quoted: "- Pactly",
+    content: "",
+  },
+  {
+    id: 6,
+    title: "“Nothing is fair.”",
+    quoted: "- The F-Bomb",
+    content: "",
+  },
+  {
+    id: 7,
+    title: "“The game is about two brothers. One is jealous of the other and they hate each other. He is kind of mentally ill and hears voices but is unsure from whom. He also has to suffer like Jesus but doesn't know why.”",
+    quoted: "- Your 'friend' BWK 2021",
+    content: "",
+  },
+  {
+    id: 8,
+    title: "“By the way 1st November it is a Public Holiday in France, so the interviewer might not be able to make it, just to update you he will be back on 31st October, so I can check his schedule again. Cheers,”",
+    quoted: "- Wil's son",
+    content: "",
+  },
+  {
+    id: 9,
+    title: "“The difference between honey and $h!t is that no matter how colorful you paint it, how much perfume you put on it - it will always be $h!t. It will never become honey.”",
+    quoted: "- The Bees",
+    content: "",
+  },
+  {
+    id: 10,
+    title: "“...but we still prefer the $h!t.”",
+    quoted: "- The Flies",
+    content: "",
+  },
+  {
+    id: 11,
+    title: "“Brothers will turn against their own brothers and hand them over to be killed. Fathers will hand over their own children to be killed. Children will fight against their own parents and will have them killed. Everyone will hate you because you follow me. But the one who remains faithful to the end will be killed too.”",
+    quoted: "- The Apocalypse happening now",
+    content: "",
+  },
+  {
+    id: 12,
+    title: "“They know you know. Just kidding.”",
+    quoted: "- Your Matrix",
+    content: "",
+  },
+  {
+    id: 13,
+    title: "“I was born more than once so I have more than one name.”",
+    quoted: "- Puppet Master",
+    content: "",
+  },
+  {
+    id: 14,
+    title: "“Identifying as 'they' is like having multipile personality disorder.”",
+    quoted: "- They Proud PhD in Psychology",
+    content: "",
+  },
+  {
+    id: 15,
+    title: "“Money can buy you almost everything - yet it has no real value.”",
+    quoted: "- Money",
+    content: "",
+  },
+  {
+    id: 16,
+    title: "“Let's start World War III and watch the whole world burn.”",
+    quoted: "- The people above",
+    content: "",
+  },
+  {
+    id: 17,
+    title: "“Parading in the streets, dressed like clowns, waving a clown flag with their d!cks out.”",
+    quoted: "- The Psychopats trying to express themselves",
+    content: "",
+  },
+  {
+    id: 18,
+    title: "“For the greater money.”",
+    quoted: "- The greater good",
+    content: "",
+  },
+  {
+    id: 19,
+    title: "“It never was about equality, it's about YOU trying to prove that you are better than everyone else.”",
+    quoted: "- Everyone else",
+    content: "",
+  },
+  {
+    id: 20,
+    title: "“Pride is a sin.”",
+    quoted: "- The Sinners",
+    content: "",
+  },
+  {
+    id: 21,
+    title: "“The biggest problem of this world is that stupid and annoying people don't know that they're stupid and annoying.”",
+    quoted: "- The ones that know",
+    content: "",
+  },
 ]
+
+
+
 
 export default thoughtEntries;
